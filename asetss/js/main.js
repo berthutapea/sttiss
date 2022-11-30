ac53064a9e8082e6e466328016af8f87bc68745d
daudsinaga1909 <daudsinaga1909@gmail.com>
2022-11-30 11:46:14 +0700

    first commit

---
+++ asetss/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $("#autoWidth").lightSlider({
+    adaptiveHeight: true,
+    auto: true,
+    item: 1,
+    slideMargin: 0,
+    loop: true,
+  });
+});
+
+/**
+* Animation on scroll
+*/
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  })
+});
+
+/**
+ * Initiate gallery lightbox 
+ */
+const galleryLightbox = GLightbox({
+  selector: '.gallery-lightbox'
+});
+
+
+const navbarHome = document.querySelector(".navbarHome");
+
+window.addEventListener("scroll", () => {
+  const post = window.scrollY > 100;
+
+  navbarHome.classList.toggle("scroll", post);
+});
